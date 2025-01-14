ec416212437309d710c3f7a5c56885d35d32e58a
Ralevski <kralevski16603@gmail.com>
2025-01-14 17:50:11 +0100

    5549

---
+++ code/5549-kristijan-ralevski/src/app/grade.util.ts/grade.util.ts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade.util.ts',
+  imports: [],
+  templateUrl: './grade.util.ts.component.html',
+  styleUrl: './grade.util.ts.component.css'
+})
+export class GradeUtilTsComponent {
+
+}
