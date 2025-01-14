b3ecfb972dda057735d504fa0f564486318b3f43
Mario Krstevski <mariokrstevski2002@gmail.com>
2025-01-14 19:15:24 +0100

    added the files

---
+++ code/5905-mario-krstevski/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar', // This should match the tag used in app.component.html
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent { }
