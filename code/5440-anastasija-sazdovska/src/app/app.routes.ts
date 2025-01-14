7f0251382bb629fa119b29657cd0c69857acd157
Anastasija <anastasijasazdovska@icloud.com>
2025-01-14 20:12:15 +0100

    Anastasija 5440

--- code/5440-anastasija-sazdovska/src/app/app.routes.ts
+++ code/5440-anastasija-sazdovska/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { StudentListComponent } from '../components/student-list/student-list.component';
+import { StudentCreateComponent } from '../components/student-create/student-create.component';
+import { StudentDetailsComponent } from '../components/student-details/student-details.component';
+import { StudentEditComponent } from '../components/student-edit/student-edit.component';
+import { AboutComponent } from '../components/about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+  { path: 'students', component: StudentListComponent },
+  { path: 'students/create', component: StudentCreateComponent },
+  { path: 'students/:id', component: StudentDetailsComponent },
+  { path: 'students/:id/edit', component: StudentEditComponent },
+  { path: 'about', component: AboutComponent }
+];
